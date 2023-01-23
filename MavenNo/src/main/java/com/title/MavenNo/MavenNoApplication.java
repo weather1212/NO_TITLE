c8313d1f1e524ecae936e216b8bb4a1265868338
weather1212 <60419960+weather1212@users.noreply.github.com>
2023-01-23 18:08:15 +0900

    2023.01.23 - init project

---
+++ MavenNo/src/main/java/com/title/MavenNo/MavenNoApplication.java
@@ -0,0 +1,13 @@
+package com.title.MavenNo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenNoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenNoApplication.class, args);
+	}
+
+}
---
+++ MavenNo/src/test/java/com/title/MavenNo/MavenNoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.title.MavenNo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenNoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
