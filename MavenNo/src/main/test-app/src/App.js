9eeb13bee5d41c173f9c1757e98cf14d2a283163
hoseong.yu <hoseong.yu@yespeech.com>
2023-02-10 17:33:49 +0900

    2023.02.10 - react 기본 문법
    - SPA 형식 이해
    - JSX 문법

--- MavenNo/src/main/test-app/src/App.js
+++ MavenNo/src/main/test-app/src/App.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React from 'react';
 
+// import {
+//     BrowserRouter as Router,
+//     Routes,
+//     Route,
 
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
+// } from 'react-router-dom';
 
-} from 'react-router-dom';
+// import LandingPage from './Components/LandingPage/LandingPage';
 
-import LandingPage from './Components/LandingPage/LandingPage';
+// (1) return시 하나의 DOM만(요소) 리턴할 수 있음
+// (2) 변수선언은 let, const 로만 해야함
+// (3) if사용 불가능 -> 삼항연산자{조건?값(true):값(false)}
+// (4) 조건부 랜더링 {조건 && 값(true)}
+// (5) css 디자인
+//    - 내부에 적는 방법
+//    - 외부 파일에 적는 방법 (class 속성 -> className)
+//    - 라이브러리 사용(bootstrap, component-styled)
 
-export default function App(){
-  return(
-      <Router>
+export default function App() {
+    let a = 10;
+    const b = 20;
+    const mystyle = {
+        color: 'red',
+    };
 
-        <Routes>
-          <Route  path="/" element={<LandingPage /> } />
-        </Routes>
-
-      </Router>
-      )
+    return (
+        <div>
+            <div style={mystyle}>
+                안녕{a === 10 ? '10입니다.' : '10이 아닙니다.'}
+            </div>
+            <h1>헤더{b === 10 && '20입니다.'}</h1>
+        </div>
+    );
 }
